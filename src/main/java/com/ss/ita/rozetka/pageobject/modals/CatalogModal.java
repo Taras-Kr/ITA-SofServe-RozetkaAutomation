ad20121f41fd5722081b9daa98d4bdcdf7ec34e3
taras.krasitskyi <taras.krasitsky@gmail.com>
2022-01-14 19:23:48 +0200

    delete all

--- src/main/java/com/ss/ita/rozetka/pageobject/modals/CatalogModal.java
+++
@@ -1,7 +0,0 @@
-package com.ss.ita.rozetka.pageobject.modals;
-
-import com.ss.ita.rozetka.pageobject.pages.HeaderPage;
-
-public class CatalogModal extends HeaderPage {
-    //for writing methods in close future
-}
